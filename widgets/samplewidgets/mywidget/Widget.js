f7dd53ea4bcdb3e88de6ac39e42b3fb834a7eb73
kpermenter <52431178+kpermenter@users.noreply.github.com>
2020-04-23 13:27:13 -0500

    first draft

---
+++ widgets/samplewidgets/mywidget/Widget.js
@@ -0,0 +1,12 @@
+// All material copyright ESRI, All Rights Reserved, unless otherwise specified.
+// See http://@sbaseurl@/jsapi/jsapi/esri/copyright.txt and http://www.arcgis.com/apps/webappbuilder/copyright.txt for details.
+
+//>>built
+define(['dojo/_base/declare', 'jimu/BaseWidget'],
+function(declare, BaseWidget) {
+  //To create a widget, you need to derive from BaseWidget.
+  return declare([BaseWidget], {
+    // Custom widget code goes here 
+    baseClass: 'jimu-widget-mywidget'
+  });
+});
